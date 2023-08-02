59a6d2b1110f1daf552d50933b69c1102f86a666
jhoanTrujillo <jhoantrujillo95@gmail.com>
2023-08-02 22:18:01 +0100

    Initial Commit

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+/* ======================================================================================================
+* Froggy Clicker is a game inspired by the popular clicker or incremental games genre. 
+* The goal is simple: click on elements on the screen to gain points, unlock upgrades, and accumulate 
+* as many points as possible to conquer the game. 
+* ====================================================================================================== */
+
